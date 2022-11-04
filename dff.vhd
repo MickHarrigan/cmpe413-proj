50dfcc3385e90da113a072c7b97cc9e9cc5d1306
dancleaver <daniel.cleaver1@gmail.com>
2022-11-03 20:19:59 -0400

    Add provided files

---
+++ dff.vhd
@@ -0,0 +1,34 @@
+--
+-- Entity: negative edge triggered D flip-flop (dff)
+-- Architecture : structural
+-- Author: 
+--
+
+library STD;
+library IEEE;                      
+use IEEE.std_logic_1164.all;       
+
+entity dff is                      
+  port ( d   : in  std_logic;
+         clk : in  std_logic;
+         q   : out std_logic;
+         qbar: out std_logic); 
+end dff;                          
+
+architecture structural of dff is 
+
+  
+begin
+  
+  output: process                 
+
+  begin                           
+    wait until ( clk'EVENT and clk = '0' ); 
+    q <= d;
+    qbar <= not d ;
+  end process output;        
+
+                             
+end structural;  
+
+
