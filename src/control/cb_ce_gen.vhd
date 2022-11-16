b1dbfeaa814f38fbe15263bffe448ea1a5d5f7d3
dancleaver <daniel.cleaver1@gmail.com>
2022-11-16 17:10:25 -0500

    Work on control modules

--- src/control/cb_ce_gen.vhd
+++ src/control/cb_ce_gen.vhd
@@ -13,11 +13,27 @@ entity cb_ce_gen is
         cb_ce_adj:  in std_logic;
         cb_ce_inv:  in std_logic;
         clk:        in std_logic;
-        ceg:        out std_logic
+        cb_ce_out:  out std_logic
     );
 end cb_ce_gen;
 
 architecture structural of cb_ce_gen is
+    component and2
+        port(
+            input1  : in std_logic;
+            input2  : in std_logic;
+            output  : out std_logic
+        );
+    end component;
+
+    component xor2
+        port(
+            input1  : in std_logic;
+            input2  : in std_logic;
+            output  : out std_logic
+        );
+    end component;
+
     component mux2
         port(
             input0  : in std_logic;
@@ -27,7 +43,17 @@ architecture structural of cb_ce_gen is
         );
     end component;
 
+    for and2_0: and2 use entity work.and2(structural);
+    for xor2_0: xor2 use entity work.xor2(structural);
+    for mux2_0: mux2 use entity work.mux2(structural);
+
+    signal cea, ceg: std_logic;
 
 begin
+    and2_0: and2 port map(cb_ce, clk, cea);
+
+    xor2_0: xor2 port map(cb_ce_inv, cea, ceg);
+
+    mux2_0: mux2 port map(ceg, cb_ce, cb_ce_adj, cb_ce_out);
 
 end structural;
