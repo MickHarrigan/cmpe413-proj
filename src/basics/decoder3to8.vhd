bccb2ea1fdfd893f7f0fa647803ccd6b749c4b56
Mick Harrigan <mharrigan328@gmail.com>
2022-11-17 21:15:10 -0500

    Create 3 to 8 decoder

---
+++ src/basics/decoder3to8.vhd
@@ -0,0 +1,58 @@
+library STD;
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity decoder3to8 is
+    port(
+        input0  : in std_logic;
+        input1  : in std_logic;
+        input2  : in std_logic;
+        output0 : out std_logic;
+        output1 : out std_logic;
+        output2 : out std_logic;
+        output3 : out std_logic;
+        output4 : out std_logic;
+        output5 : out std_logic;
+        output6 : out std_logic;
+        output7 : out std_logic
+    );
+end decoder3to8;
+
+architecture structural of decoder3to8 is
+    component and3
+        port(
+            input1  : in std_logic;
+            input2  : in std_logic;
+            input3  : in std_logic;
+            output  : out std_logic
+        );
+    end component;
+
+    component inverter
+        port(
+            input   : in std_logic;
+            output  : out std_logic
+        );
+    end component;
+
+    for and3_0, and3_1, and3_2, and3_3, and3_4, and3_5, and3_6, and3_7: and3 use entity work.and3(structural);
+    for inv_0, inv_1, inv_2: inverter use entity work.inverter(structural);
+
+    signal in0n, in1n, in2n: std_logic;
+
+begin
+
+    inv_0: inverter port map(input0, in0n);
+    inv_1: inverter port map(input1, in1n);
+    inv_2: inverter port map(input2, in2n);
+
+    and3_0: and3 port map(in0n, in1n, in2n, output0);
+    and3_1: and3 port map(input0, in1n, in2n, output1);
+    and3_2: and3 port map(in0n, input1, in2n, output2);
+    and3_3: and3 port map(input0, input1, in2n, output3);
+    and3_4: and3 port map(in0n, in1n, input2, output4);
+    and3_5: and3 port map(input0, in1n, input2, output5);
+    and3_6: and3 port map(in0n, input1, input2, output6);
+    and3_7: and3 port map(input0, input1, input2, output7);
+
+end structural;
